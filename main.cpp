a72b1e5847ca4ade30696c3980bf074db4ed2c51
Zhang-fulai <129665815+Zhang-fulai@users.noreply.github.com>
2023-05-03 09:55:17 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS 1 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Hello World" << endl;
+	}
+	return 0;
+}
